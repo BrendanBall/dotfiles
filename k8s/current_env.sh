af1d76a75a96e868c2bd2bc9e94add65359b1af3
Brendan Ball <brendan@nomanini.com>
2018-04-16 07:23:30 +0200

    fix i3status k8s env block

--- k8s/current_env.sh
+++ k8s/current_env.sh
@@ -2,10 +2,10 @@
 CONTEXT=$(kubectl config current-context)
 if [ "$CONTEXT" == "gke_nomanini-dashboard_us-central1-a_dev" ]
 then
-    echo DEV
+    echo '{"state": "Idle", "text": "DEV"}'
 elif [ "$CONTEXT" == "gke_nomanini-dashboard_us-central1-a_prod" ]
 then
-    echo PROD
+     echo '{"state": "Critical", "text": "PROD"}'
 else
-    echo "$CONTEXT"
+     echo "{\"state\": \"Idle\", \"text\": \"$CONTEXT\"}"
 fi
