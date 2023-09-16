c03303035bddbbac62dba909d4ed7eaca1ecfda6
Brendan Ball <ball.brendan50@gmail.com>
2023-09-16 10:44:57 +0200

    remove old config

--- cloudsql/status-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-STATUS=$(systemctl --user is-active cloudsql-dev)
-
-echo "{\"state\": \"Idle\", \"text\": \"SQL DEV: $STATUS\"}"
--- k8s/current_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CONTEXT=$(kubectl config current-context)
-STATE=Idle
-if [ "$CONTEXT" == "prod" ]
-then
-     STATE=Critical
-fi
-
-echo "{\"state\": \"${STATE}\", \"text\": \"${CONTEXT^^}\"}"
